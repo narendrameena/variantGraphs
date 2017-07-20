c5cedb1c527b7f2b2fa3d169fde31b6ba847e998
Narendra Meena <narendrabhagri03@gmail.com>
2017-07-20 16:12:46 +0530

    install maftools
    insatalling maftools and other dependencies

--- graphsMafTools.R
+++ graphsMafTools.R
@@ -2,5 +2,23 @@
 #description making graphs of benchmarking results using annovar and rtg-tools results 
 
 ## try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-biocLite('maftools')
+#Install Bioconductor dependencies.
+source("http://bioconductor.org/biocLite.R")
+biocLite("ComplexHeatmap")
+biocLite("VariantAnnotation")
+biocLite("Biostrings")
+
+#Install maftools from github repository.
+library("devtools")
+install_github(repo = "PoisonAlien/maftools")
+
+
+var.annovar = system.file("extdata", "variants.hg19_multianno.txt", package = "maftools")
+
+
+##annovar output into MAF using annovarToMaf
+var.annovar.maf = annovarToMaf(annovar = var.annovar, Center = 'CSI-NUS', refBuild = 'hg19', 
+                               tsbCol = 'Tumor_Sample_Barcode', table = 'ensGene')
+## Converting Ensemble Gene IDs into HGNC gene symbols.
+
+## Done! Original ensemble gene IDs are preserved under field name ens_id
