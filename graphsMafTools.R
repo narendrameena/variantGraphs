e4b4857b58355c9186329efc1c259e67731e33d9
Narendra Meena <narendrabhagri03@gmail.com>
2017-07-20 15:43:11 +0530

    start
    creat a directory for variant braphical analysis

---
+++ graphsMafTools.R
@@ -0,0 +1,6 @@
+#author narumeena
+#description making graphs of benchmarking results using annovar and rtg-tools results 
+
+## try http:// if https:// URLs are not supported
+source("https://bioconductor.org/biocLite.R")
+biocLite('maftools')
